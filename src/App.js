14f5436c891dbaf66477d42667a5b8bc40fe2fff
chang-hoon <mythmyth128@naver.com>
2021-01-31 11:15:33 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers =
+[
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '허창훈',
+  'birthday' : '721228',
+  'gender' : '남자',
+  'job' : '직장인'
+},
+
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '821228',
+  'gender' : '남자',
+  'job' : '직장인'
+},
+
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '이순신',
+  'birthday' : '921228',
+  'gender' : '남자',
+  'job' : '직장인'
+}
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="gray-backgroud">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system !</h2>
-      </div>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+        )} )
+      }
     </div>
   );
 }
